d6f981d7e560cc914e99c6e7d2d732fec7adcf80
Krishan Samarawickrama <Krishans1990@live.com>
2022-07-07 21:33:29 +0530

    Add FileStreamLogger adapter

---
+++ Adapter/FileStreamILoggerAdapter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using Microsoft.Extensions.Logging;
+
+var logger = new FileLogger<Program>("Log.txt");
+
+for (int i = 1; i <= 10; i++)
+{
+    logger.LogDebug($"{i} - This is a new log message. at {DateTime.Now.ToString("yyyy-MMMM-dd hh:mm:ss t z")}");
+}
+
+
+public class FileLogger<T> : FileStream, ILogger
+{
+    public FileLogger(string path) : base(path, FileMode.Append)
+    {
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return true;
+    }
+
+    public IDisposable BeginScope<TState>(TState state)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception,
+        Func<TState, Exception?, string> formatter)
+    {
+        byte[] byteArray = Encoding.UTF8.GetBytes(state?.ToString() + "\n");
+
+        Write(byteArray, 0, byteArray.Length);
+        Flush();
+    }
+}
